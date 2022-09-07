074d8aebad83d31ca0ca271403fc6dc97a2b654a
Maria Piper <mbtomori@users.noreply.github.com>
2022-09-07 16:03:29 -0700

    Initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Maria Piper's CS 5610 Tuiter Web App
         </p>
         <a
           className="App-link"
